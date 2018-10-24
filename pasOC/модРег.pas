9c7ea98cbe0707864bec1755f120c3126d2aec5d
prospero78su <prospero.78.su@gmail.com>
2018-10-24 15:46:22 +0300

    021 Добавление метода для присвоения значения регистру

--- pasOC/модРег.pas
+++ pasOC/модРег.pas
@@ -6,9 +6,25 @@
 type
    ///Основа для всех типов регистров
    тРег = class
+   private 
+      _знач: int64 := 0;
+      ///Приватный метод для установки значения регистра
+      procedure значУст(пЗнач: int64);
+      begin
+         if пЗнач > integer.MaxValue then
+            raise new Exception('тРег._Знач_Уст: слишком большое значение пЗнач ' + пЗнач.ToString);
+         if пЗнач < integer.MinValue then
+            raise new Exception('тРег._Знач_Уст: слишком малькое значение пЗнач ' + пЗнач.ToString);
+         self._знач := пЗнач;
+      end;
+   
+   public 
       constructor Create;
       begin
          Writeln('тРег.Create')
       end;
+      
+      property знач: int64 read self._знач write значУст;
+   
    end;
 end.
