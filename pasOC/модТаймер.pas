185e2e58184f36c684d06ca8a48cbf344018e72c
prospero78su <prospero.78.su@gmail.com>
2018-10-24 14:46:19 +0300

    008 Добавлен главный таймер системы.

---
+++ pasOC/модТаймер.pas
@@ -0,0 +1,40 @@
+﻿unit модТаймер;
+{
+   Предоставляет системный таймер для тактирования всей системы.
+}
+uses Timers;
+
+type
+   тТаймер = class(Timer)
+   private 
+      ///Интервал работы таймера в миллисекундах
+      _интервал: integer;
+      
+      ///Процедура для вызова при срабатывании таймера
+      _проц: procedure;
+   
+   public 
+      ///Конструктор таймера:
+      ///   пМС -- интервал в мс
+      ///   пПроц -- процедура для вызова
+      constructor Create(пМС: integer; пПроц: procedure);
+      begin
+         self._интервал := пМС;
+         self._проц := пПроц;
+      end;
+      
+      ///Возвращает интервал срабатывания таймера, мс
+      property интервал: integer read self._интервал;
+      
+      procedure Старт;
+      begin
+         self.Start;
+      end;
+      
+      procedure Стоп;
+      begin
+         self.Stop;
+      end;
+   
+   end;
+end.
