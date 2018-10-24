86c9cb6e4523c8fedf18c5c012505cd49b3960ab
prospero78su <prospero.78.su@gmail.com>
2018-10-24 15:31:13 +0300

    020 Добавлена база для всех регистров

---
+++ pasOC/модРег.pas
@@ -0,0 +1,14 @@
+﻿unit модРег;
+{
+Предоставляет тип для основы регистра
+}
+
+type
+   ///Основа для всех типов регистров
+   тРег = class
+      constructor Create;
+      begin
+         Writeln('тРег.Create')
+      end;
+   end;
+end.
