75c14e1b5bc5bc3d9ac484574f1617026612d3d2
prospero78su <prospero.78.su@gmail.com>
2018-10-24 15:06:56 +0300

    017 Добавление тЦП в OberonVM

---
+++ pasOC/модЦП.pas
@@ -0,0 +1,14 @@
+﻿unit модЦП;
+{
+   Модуль предоставляет тип центрального процессора для OberonVM.
+}
+
+type
+   ///Тип эмулирующий центральный процессор ПК
+   тЦП = class
+      constructor Create;
+      begin
+         Writeln('тЦП.Create');
+      end;
+   end;
+end.
