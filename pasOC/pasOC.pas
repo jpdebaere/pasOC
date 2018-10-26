868bb2c2cd95087e92ccdcf6a35f0ef2498dc3ce
prospero78su <prospero.78.su@gmail.com>
2018-10-26 10:03:59 +0300

    027 Добавление вывода шапки

--- pasOC/pasOC.pas
+++ pasOC/pasOC.pas
@@ -1,4 +1,4 @@
-﻿uses модПК;
+﻿uses модКонсоль, модПК;
 {$product 'OberonVM'}
 {$version '2018-10-24 0022'}
 {$company 'KBK Technicks ltd.'}
@@ -6,5 +6,6 @@
 {$trademark 'BSD-2 Clause'}
 begin
    var пк := модПК.тПК.Create();
+   конс.Шапка;
    пк.Старт;
 end.
