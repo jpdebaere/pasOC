b3b0c6a34d6a2d9a7a58148400b08756d76f375e
prospero78su <prospero.78.su@gmail.com>
2018-10-26 10:03:41 +0300

    026 Добавление консоли для вывода служебной информации

---
+++ pasOC/модКонсоль.pas
@@ -0,0 +1,45 @@
+﻿unit модКонсоль;
+{
+Предоставляет возможность выводить вконсоль централизованно.
+}
+
+uses GraphWPF;
+
+type
+   ///Тип консоли, обеспечивает вывод различного рода информации в консоль.
+   ///Объект-одиночка.
+   тКонс = class
+   private 
+      procedure _Строка(пСтрока: string);
+      begin
+         System.Console.Write('           ');
+         System.Console.BackgroundColor := System.ConsoleColor(9);
+         System.Console.ForegroundColor := System.ConsoleColor(15);
+         System.Console.WriteLine(пСтрока);
+         System.Console.ResetColor;
+      end;
+   
+   public 
+      constructor Create;
+      begin
+      end;
+      ///Выводит шапку виртуальной машины в консоль
+      procedure Шапка;
+      begin
+         self._Строка('');
+         self._Строка('+-------------------------------+');
+         self._Строка('|           OberonVM            |');
+         self._Строка('| KBK Technics ltd.    BSD-2    |');
+         self._Строка('| 2018-10-26 10:01   Build 0025 |');
+         self._Строка('+-------------------------------+');
+         self._Строка('');
+      end;
+   end;
+
+var
+   конс: тКонс;
+
+begin
+   конс := тКонс.Create();
+end.
+
