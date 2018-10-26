2f6268a02e41f3a8eebae196e3e3331bc0d73214
prospero78su <prospero.78.su@gmail.com>
2018-10-26 13:54:53 +0300

    029 Добавление типа регистра общего назначения

---
+++ pasOC/модРон.pas
@@ -0,0 +1,28 @@
+﻿unit модРон;
+{
+   Модуль обеспечивает регистром общего назначения процессор
+}
+interface
+
+uses модРег;
+
+type
+   тРон = class(модРег.тРег)
+   private 
+      _ноль: boolean;
+      _отриц: boolean;
+      _перенос: boolean;
+   public 
+      constructor Create;
+      begin
+         inherited Create;
+         self.знач := 0;
+         self._ноль:=true;
+         self._отриц:=false;
+         self._перенос:=false;
+      end;
+   end;
+
+implementation
+
+end.
